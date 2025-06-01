5040571e7dd48ae49a6f5e260cbbb3593f0d0104
JoPhil15 <joanna.philip03@gmail.com>
2025-06-02 00:03:36 +0530

    Create unsignedhighest.c

---
+++ code/Day1/unsignedhighest.c
@@ -0,0 +1,7 @@
+#include<stdio.h> 
+#include<math.h> 
+int main() 
+{//highest no that can be rep = 2^64 
+unsigned long long int max = (unsigned ong long int) (pow(2,64) -1);//datatype for double word 
+printf("HIghesr number represented by unsigned long long int is %llu \n", max); 
+return 0;
